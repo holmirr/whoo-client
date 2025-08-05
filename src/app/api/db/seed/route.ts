043c502440aaf571313b4364c8722fde93d856f5
holmirr <84669138+holmirr@users.noreply.github.com>
2025-08-05 10:06:44 +0900

    Update route.ts

--- src/app/api/db/seed/route.ts
+++ src/app/api/db/seed/route.ts
@@ -2,6 +2,8 @@ import { seedTable } from "@/libs/database";
 
 export async function GET(request: Request) {
   try {
+    // seedTable() では,whoo_usersのtableを削除してから、再度同名のtableを作成する。
+    // columnsの内容や構成を変更する際に使用
     await seedTable();
     return new Response("OK");
   } catch (error) {
